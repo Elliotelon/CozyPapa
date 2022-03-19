1d3dfb4b6fd8f2710ea5108c76adb908e52d589c
kimminkyu <elliotelon@gmail.com>
2022-03-19 14:58:34 +0900

    소개 페이지

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Minim laboris est qui anim ut eu non sint cupidatat cillum aute. Sit
+            nulla duis et officia. Et ut occaecat nostrud in reprehenderit enim
+            culpa eiusmod ex consectetur. Officia ut cupidatat mollit nulla esse
+            consectetur.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +52,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;

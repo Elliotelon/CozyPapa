c156b1520235b494c7c71259bec9eb4a2c71f735
kimminkyu <elliotelon@gmail.com>
2022-03-20 15:39:57 +0900

    상품 페이지 (리스트 뷰)

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import { useFilterContext } from '../context/filter_context'
-import GridView from './GridView'
-import ListView from './ListView'
+import React from "react";
+import { useFilterContext } from "../context/filter_context";
+import GridView from "./GridView";
+import ListView from "./ListView";
 
 const ProductList = () => {
-  const { filtered_products: products} = useFilterContext();
+  const { filtered_products: products, grid_view } = useFilterContext();
 
-  return <GridView products={products}>product list</GridView>
-}
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: "none" }}>
+        Sorry, no products matched your search...
+      </h5>
+    );
+  }
+  if (grid_view === false) {
+    return <ListView products={products} />
+  }
 
-export default ProductList
+  return <GridView products={products}>product list</GridView>;
+};
+
+export default ProductList;

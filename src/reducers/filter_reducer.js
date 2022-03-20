f9282ee9ee8d948403c9d93ba21e36de90021efd
kimminkyu <elliotelon@gmail.com>
2022-03-20 17:21:06 +0900

    상품 페이지(정렬 셀렉트 기능)

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -17,14 +17,43 @@ const filter_reducer = (state, action) => {
       filtered_products: [...action.payload],
     };
   }
-  if(action.type === SET_GRIDVIEW) {
-    return {...state, grid_view: true}
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true };
   }
-  if(action.type === SET_LISTVIEW) {
-    return {...state, grid_view: false}
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false };
   }
-  if(action.type === UPDATE_SORT) {
-    return {...state, sort: action.payload}
+  if (action.type === UPDATE_SORT) {
+    return { ...state, sort: action.payload };
+  }
+  if (action.type === SORT_PRODUCTS) {
+    const { sort, filtered_products } = state;
+    let tempProducts = [...filtered_products];
+    if (sort === "price-lowest") {
+      tempProducts = tempProducts.sort((curProduct, nextProduct) => {
+        if (curProduct.price < nextProduct.price) {
+          return -1;
+        }
+        if (curProduct.price > nextProduct.price) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+    if (sort === "price-highest") {
+      tempProducts = tempProducts.sort((curProduct, nextProduct) => nextProduct.price - curProduct.price);
+    }
+    if (sort === "name-a") {
+      tempProducts = tempProducts.sort((curProduct, nextProduct) => {
+        return curProduct.name.localeCompare(nextProduct.name);
+      });
+    }
+    if (sort === "name-z") {
+      tempProducts = tempProducts.sort((curProduct, nextProduct) => {
+        return nextProduct.name.localeCompare(curProduct.name);
+      });
+    }
+    return { ...state, filtered_products: tempProducts };
   }
   return state;
   throw new Error(`No Matching "${action.type}" - action type`);

9d6c5e6a5fc94c0a754b751780510346b0321692
kimminkyu <elliotelon@gmail.com>
2022-03-20 13:45:27 +0900

    상품 디테일 페이지 (상품 색상 선택)

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -86,7 +86,7 @@ const SingleProductPage = () => {
             {company}
           </p>
           <hr />
-          {stock > 0 && <AddToCart />}
+          {stock > 0 && <AddToCart product={product} />}
         </section>
       </div>
     </div>

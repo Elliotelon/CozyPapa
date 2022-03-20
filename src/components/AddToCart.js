7b6170dcecc5b78979578675798a865032efed16
kimminkyu <elliotelon@gmail.com>
2022-03-20 14:30:09 +0900

    상품 디테일 페이지 (수량 버튼)

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -8,6 +8,27 @@ import AmountButtons from "./AmountButtons";
 const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] =useState(1);
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1
+      if(tempAmount > stock) {
+        tempAmount = stock
+      }
+      return tempAmount
+    })
+  }
+
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1
+      if(tempAmount < 1) {
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+  }
 
   return (
     <Wrapper>
@@ -30,7 +51,12 @@ const AddToCart = ({ product }) => {
           })}
         </div>
       </div>
-      <div className="btn-container"></div>
+      <div className="btn-container">
+        <AmountButtons amount={amount} increase={increase} decrease={decrease} />
+        <Link to="/cart" className="btn">
+          add to cart
+        </Link>
+      </div>
     </Wrapper>
   );
 };

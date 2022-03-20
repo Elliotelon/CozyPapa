c3c2d664794484b71567daa3aebc71cfab3010ac
kimminkyu <elliotelon@gmail.com>
2022-03-20 21:52:46 +0900

    상품 페이지(필터 색상)

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -81,8 +81,35 @@ const Filters = () => {
               })}
             </select>
           </div>
-
           {/* end of companies */}
+          {/* colors */}
+          <div className="form-control">
+            <h5>colors</h5>
+            <div className="colors">
+              {colors.map((c, index) => {
+                if(c === "all") {
+                  return <button name="color" onClick={updateFilters} data-color="all" className={`${color === "all" ? "all-btn active" : "all-btn"}`}>
+                    all
+                  </button>
+                }
+                return (
+                  <button
+                    key={index}
+                    name="color"
+                    style={{ background: c }}
+                    className={`${
+                      color === c ? "color-btn active" : "color-btn"
+                    }`}
+                    data-color={c}
+                    onClick={updateFilters}
+                  >
+                    {color === c ? <FaCheck /> : null}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          {/* end of colors */}
         </form>
       </div>
     </Wrapper>

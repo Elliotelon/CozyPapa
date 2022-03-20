610f93d320b8f9441b8c87ee360272439b4e10d1
kimminkyu <elliotelon@gmail.com>
2022-03-20 21:23:40 +0900

    상품 페이지(필터 브랜드)

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -62,7 +62,27 @@ const Filters = () => {
               })}
             </div>
           </div>
-          {/* end search input */}
+          {/* end of categories */}
+          {/* companies */}
+          <div className="form-control">
+            <h5>company</h5>
+            <select
+              name="company"
+              value={company}
+              onChange={updateFilters}
+              className="company"
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+
+          {/* end of companies */}
         </form>
       </div>
     </Wrapper>

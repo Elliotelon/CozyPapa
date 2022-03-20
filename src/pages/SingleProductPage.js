a89ca349ebf13fe94af93524ecf876214024c895
kimminkyu <elliotelon@gmail.com>
2022-03-20 13:22:36 +0900

    상품 디테일 페이지 (별점)

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -56,6 +56,8 @@ const SingleProductPage = () => {
     images,
   } = product;
 
+  console.log(stars)
+
   return <Wrapper>
     <PageHero title={name} product />
     <div className="section section-center page">
@@ -66,7 +68,7 @@ const SingleProductPage = () => {
         <ProductImages images={images} />
         <section className="content">
           <h2>{name}</h2>
-          <Stars />
+          <Stars stars={stars} reviews={reviews} />
           <h5 className="price">{formatPrice(price)}</h5>
           <p className="desc">
             {description}

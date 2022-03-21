a2a39f3eb498c0381f70e18117aeecc89261cd80
kimminkyu <elliotelon@gmail.com>
2022-03-21 23:56:16 +0900

    계정(Auth0 - AuthWrapper)

--- src/App.js
+++ src/App.js
@@ -10,36 +10,39 @@ import {
   About,
   Produts,
   Private,
+  AuthWrapper,
 } from "./pages";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/cart">
-          <Cart />
-        </Route>
-        <Route exact path="/products">
-          <Produts />
-        </Route>
-        <Route exact path="/products/:id" children={<SingleProduct />} />
-        <Private exact path="/checkout">
-          <Checkout />
-        </Private>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+          <Route exact path="/products">
+            <Produts />
+          </Route>
+          <Route exact path="/products/:id" children={<SingleProduct />} />
+          <Private exact path="/checkout">
+            <Checkout />
+          </Private>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-// will remove later
-import { useUserContext } from '../context/user_context';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const PrivateRoute = ({children, ...rest}) => {
-  console.log({...rest})
-  const {myUser } = useUserContext();
+const PrivateRoute = ({ children, ...rest }) => {
+  const { user } = useAuth0();
 
-  return <Route {...rest} render={() => {
-    return myUser ? children : <Redirect to="/" />
-  }} ></Route>;
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return user ? children : <Redirect to="/" />;
+      }}
+    ></Route>
+  );
 };
 export default PrivateRoute;
